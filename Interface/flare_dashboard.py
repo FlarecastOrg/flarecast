6d000a5889ba61567882618b67fe0e6f488a0550
Selina <guan.s45@gmail.com>
2025-06-21 14:15:29 -0700

    trail run 4

--- Interface/flare_dashboard.py
+++ Interface/flare_dashboard.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title("🔥 Flare Power Monitor rev3")
+st.title("🔥 Flare Power Monitor rev4")
 
 # Load CSV
-df = pd.read_csv("Simulated_Ground_Sensor_Data.csv")  # ✅ This is enough
-print(df.head())  # Check if it's loaded correctly
+df = pd.read_csv("Simulated_Ground_Sensor_Data.csv")
