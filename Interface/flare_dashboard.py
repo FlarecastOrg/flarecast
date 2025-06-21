ad1c45f2bb248944447486ddd39ef040920bbf42
Selina <guan.s45@gmail.com>
2025-06-21 14:08:14 -0700

    trial run

--- Interface/flare_dashboard.py
+++ Interface/flare_dashboard.py
@@ -2,16 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title("🔥 Flare Power Monitor")
+st.title("🔥 Flare Power Monitor rev2")
 
 uploaded_file = st.file_uploader("Simulated_Ground_Sensor_Data", type="csv")
-
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-    st.write("Preview:", df.head())
-
-    min_temp = st.slider("Min flame temp (°C)", 500, 1100, 800)
-    filtered = df[df["flame_temp_C"] >= min_temp]
-
-    # Fixed line
-    st.line_chart(filtered.set_index("timestamp")[["power_MW"]])
