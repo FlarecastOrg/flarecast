c9b27fe08045ddb798aae141f6ecdc53da5fdafb
Selina <guan.s45@gmail.com>
2025-06-21 14:54:05 -0700

    download stuff

--- Interface/flare_dashboard.py
+++ Interface/flare_dashboard.py
@@ -6,3 +6,13 @@ st.title("🔥 Flare Power Monitor rev4")
 
 # Load CSV
 df = pd.read_csv("Simulated_Ground_Sensor_Data.csv")
+
+df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+# Show preview
+st.subheader("Data Preview")
+st.write(df.head())
+
+# Show full dataset
+st.subheader("Full Dataset")
+st.dataframe(df)
